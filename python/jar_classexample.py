837d5634326a190961c98ef00cf683993a9449ae
obsastro1 <obsastro1@COMA.PHYSICS.SIENA.EDU>
2013-05-31 12:19:08 -0400

    My first python code

---
+++ python/jar_classexample.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+from pylab import *
+import atpy
+
+class cluster:
+    def __init__(self, clustername):
+        infile = '/home/rfinn/research/LocalClusters/NSAmastertables/'+clustername+'_NSAmastertable.fits'
+        self.mdat = atpy.Table(infile)
+
+    def plotradec(self):
+        figure()
+        plot(self.mdat.RA,self.mdat.DEC,'b.')
+
+    
+        
