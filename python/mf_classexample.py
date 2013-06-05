c10b379bd1729007728a7e4648106339c4d9e3bc
obsastro4 <obsastro4@COMA.PHYSICS.SIENA.EDU>
2013-06-05 11:05:05 -0400

    absgbandmag.png

--- python/mf_classexample.py
+++ python/mf_classexample.py
@@ -10,4 +10,27 @@ class cluster:
     def plotradec(self):
         figure()
         plot(self.mdat.RA,self.mdat.DEC,'b.')
-            
+    def plotredshift(self):
+        figure()
+        hist(self.mdat.ZDIST)
+        title('RedShift MKW11')
+        
+
+class nasasloan:
+    def __init__(self):
+        infile='/home/share/research/nsa/nsa_v0_1_2topcat.fits'
+        #infile='/home/share/research/nsa/NSA_LCSregion.fits'
+        self.ndat=atpy.Table(infile)
+    def absgbandmag(self):
+        figure()
+        hist(self.ndat.ABSMAG[:,3],bins=100)
+        title('Absolute g-band Magnitude (NSA_LCS Region) ')
+        xlim(xmax=-7)
+        xlim(xmin=-25) 
+        ax=gca()
+        ax.set_yscale('log')
+        xlabel('Absolute g-band Magnitude')
+        ylabel('Number of Galaxies')
+
+nsa=nasasloan()
+
