30f02cdc9a8f2480f85062badd57e0f112cf1dc6
John Moustakas <ioannis@COMA.PHYSICS.SIENA.EDU>
2013-06-06 11:58:46 -0400

    more cutout code

--- python/moustakas/cutout.py
+++ python/moustakas/cutout.py
@@ -7,29 +7,18 @@ from astLib import astWCS
 from astLib import astImages
 
 def cutout():
-    path = '/Users/ioannis/archive/macs1206/HST/images/mosaicdrizzle_image_pipeline/scale_65mas/'
-    file = 'macs1206_mosaic_065mas_wfc3ir_f105w_drz_20110815.fits.gz'
+    path = '/home/ioannis/'
+    myfile = 'macs1149_f105w.fits.gz'
 
-    im = pyfits.getdata(path+file)
-    hdr = pyfits.getheader(path+file)
+    im = pyfits.getdata(path+myfile)
+    hdr = pyfits.getheader(path+myfile)
     sz = im.shape
     
     wcs = astWCS.WCS(hdr,mode='pyfits')
 
 # get the celestial coordinates at the center
     radec = wcs.pix2wcs(sz[0]/2,sz[1]/2)
-    width = 20.0/3600.0 # degrees
-    cutim = astImages.clipImageSectionWCS(im,wcs,radec[0],\
-                                              radec[1],width,returnWCS=True)
-    out = numpy.array(cutim['clippedSection'])
+    width = 60.0/3600.0 # degrees
+    cutim = astImages.clipImageSectionWCS(im,wcs,radec[0],radec[1],width,returnWCS=True)
 
-    astImages.saveBitmap('junk.png',out,'smart','1024','grey')
-
-    astImages.saveBitmap('junk.fits',im,'smart','1024','grey')
-    
-
-    
-    cutout = astImages.clipImageSectionPix(im,2500,2500,[200,200])
-    pyfits.writeto('junk.fits',fim,hdr) # create new fits file
-
-    saveFITS('junk.fits',cutim,imageWCS=wcs)
+    astImages.saveBitmap('junk.png',cutim['data'],[0.0,0.5],300,'gray')
