70afa568e6ab9c4e6236caf581962dd474bdc193
mf06engl <mf06engl@siena.edu>
2013-06-13 10:31:28 -0400

    mfe

--- python/mf_classexample.py
+++ python/mf_classexample.py
@@ -24,28 +24,27 @@ class nasasloan:
     def gbandabsmag(self):
         figure()
         hist(self.ndat.ABSMAG[:,3],bins=100)
-        title('Absolute Magnitude( g-band)from WISE ') 
+        #title('Absolute Magnitude( g-band)from WISE ') 
         ax=gca()
         ax.set_yscale('log')
-        xlabel('Absolute g-band Magnitude')
-        ylabel('Number of Galaxies')
-    def gbandlum(self):
+        xlabel('M')
+        ylabel('N')
+    def gbandlum(self): 
         figure()
         self.amag=self.ndat.ABSMAG[:,3]
         print self.amag
-        # self.luminosity=(3.839*(10.**26.))*(10.**(((self.amag)-4.83)/-2.5))
-        #loglum= log10(self.luminosity)
         self.luminosity=(self.amag-4.83)/-2.5
         print 'this is a test '
         print self.luminosity
-        hist(self.luminosity,bins=500,)
-        # hist(self.luminosity,bins=100,)
+        hist(self.luminosity,bins=500,range=[5,12])
+        #hist(self.luminosity,bins=100,)
         # ax=gca()
         # ax.set_yscale('log')
         xlim(5,12)
         legend(['WISE Data'], loc='upper left')
-        xlabel(' log(g-band L/L${_\odot}$)') 
+        xlabel(' log(g-band L/L$_\odot$)') 
         ylabel('N')
-        
+
 
 nsa=nasasloan()
+    
