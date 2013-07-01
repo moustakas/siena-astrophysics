c636eec25ba4b42e5c73faf91802493f3040e0c9
joey-rowley <trackmaster138@aim.com>
2013-07-01 14:18:47 -0400

    updated SED plot

--- python/joey/BCG_plot.py
+++ python/joey/BCG_plot.py
@@ -1,8 +1,22 @@
 #! /usr/bin/env python
 
+from pylab import *
 import matplotlib.pyplot as plt
 import numpy as np
 import sextutils as se
+from scipy import optimize
+
+
+def fitfunc(p,x):
+    ret = p[1]*x + p[0]
+    return ret
+
+
+def errfunc(p,x,y,yerr):
+    ret = (((fitfunc(p,x)-y)**2)/yerr**2).sum()
+    return ret
+
+
 
 def bcg_plot(clustername):
     datapath = '/home/obsastro1/siena-astrophysics/summer2013/clash/sbprofiles/'
@@ -14,13 +28,30 @@ def bcg_plot(clustername):
     for i in range(len(cat.sma)):
          x = cat.sma[i]
          y = cat.f160w[i]
-         plt.plot(x,y,'o')
-
-         plt.xlabel('SMA')
-         plt.ylabel('F160W')
-         plt.savefig(savepath)
+         plt.plot(x,y,'bo')
+         
+    ax = plt.gca()
+    ax.set_ylim(ax.get_ylim()[::-1])
+    
+    plt.title('Light Profile')
+    plt.xlabel('SMA')
+    plt.ylabel('F160W')
+    plt.savefig(savepath)
 
     plt.show()
+
+
+
+def best_fit():
+    yerr = np.ones(len(x))
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.errorbar(x,y,yerr= yerr,fmt='o')
+
+    params_starting_vals = [1.0,1.0]
+    params_final_vals = optimize.fmin(errfunc,params_starting_vals[:],args=(x,y,yerr),full_output=True)
+
+
     
 if __name__ == '__main__':
     clustername = 'a1423'
