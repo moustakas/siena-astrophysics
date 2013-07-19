602a9be8c0d2a454b0442442c7e43edde23b07cb
thejackparticle <je08roge@siena.edu>
2013-07-19 15:05:02 -0400

    updates on code

--- python/jack/BCG_cutout.py
+++ python/jack/BCG_cutout.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 
+# montage -label 'MACS0329' macs0329-bcg.png -label 'MACS1206' macs1206-bcg.png junk.png
+
 import sextutils as se
 import numpy as np
 import pyfits
@@ -8,6 +10,7 @@ from astLib import astImages
 from scipy.misc import imread
 import glob
 import Image
+import math
 
 def bcg_cutout(clustername,ra,dec,dirname,width):
     #read image, header
@@ -24,20 +27,25 @@ def bcg_cutout(clustername,ra,dec,dirname,width):
     wcs = astWCS.WCS(hdr,mode='pyfits')
     #slice image
 
-#    print width
-    ra_left = ra+width
-    ra_right = ra-width
-    dec_top = dec-width
-    dec_bottom = dec+width
+    rawidth = width/math.cos(math.radians(dec))
+    decwidth = width
+#    print rawidth, decwidth
+    ra_left = ra+rawidth
+    ra_right = ra-rawidth
+    dec_top = dec+decwidth
+    dec_bottom = dec-decwidth
 #    print ra_left, ra_right, dec_top, dec_bottom
 
     xy_upperleft = wcs.wcs2pix(ra_left,dec_top)
     xy_bottomright = wcs.wcs2pix(ra_right,dec_bottom)
 #    print xy_upperleft, xy_bottomright
+#    print xy_upperleft[0]-xy_bottomright[0]
+#    print xy_upperleft[1]-xy_bottomright[1]
+#    print 2*3600.0*width/0.065
     
 #   box = tuple([2400,2300,2700,2600])
-    box = tuple([int(xy_upperleft[0]),int(xy_upperleft[1]),
-                 int(xy_bottomright[0]),int(xy_bottomright[1])])
+    box = tuple([int(xy_upperleft[0]),5000-int(xy_upperleft[1]),
+                 int(xy_bottomright[0]),5000-int(xy_bottomright[1])])
 #    print box
     cutbcg = png.crop(box)
     
@@ -50,9 +58,10 @@ if __name__ == "__main__":
     bcg = se.se_catalog(bcgfile)    
     outpath = '/home/obsastro2/clash/'
     #loop BCG in BCG file
-    for i in range (1):
-#    for i in range (len(bcg.ra)):
-        outfile = outpath+str(bcg.shortname[i])+'-bcg_test.png'
+#    for j in range (1):
+#        i = 16
+    for i in range (len(bcg.ra)):
+        outfile = outpath+str(bcg.shortname[i])+'-bcg.png'
         print i, outfile
                 
     #call bcg_cutout
