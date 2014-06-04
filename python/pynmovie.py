a9e6b298e1927cc002e0c715d3cf2fcb5bf262d9
moustakas <john.moustakas@gmail.com>
2014-06-04 15:00:24 -0400

    added fancier movie options

--- python/pynmovie.py
+++ python/pynmovie.py
@@ -5,6 +5,7 @@ import pynbody.analysis.angmom as angmom
 import pynbody as pyn
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
+import numpy as np
 #import pynbody.plot as p_plt
 
 if __name__ == "__main__":
@@ -27,61 +28,64 @@ if __name__ == "__main__":
                 (vmin, vmax) = (None, None)
                 for i in args:
                         # load the snapshot and retrieve the xyz coordinates
+			print 'Reading ', i
                         sim = pyn.load(i)
                         xyz_dm = sim.dm['pos']
                         xyz_stars = sim.stars['pos']
                         xyz_gas = sim.gas['pos']
-                        
+
+# figure out the range
+			if imgcount==0:
+				width = np.max(np.abs(xyz_stars))*1.1
                         # build the multi-panel plot                
-                        
                         plt.figure(1)
                         # xy, yz, xz for the stars
                         plt.subplot(2,3,1)
-                        plt.hist2d(xyz_stars[:,0],xyz_stars[:,1],bins=50,cmap='Greys',norm=LogNorm())
+                        plt.hist2d(xyz_stars[:,0],xyz_stars[:,1],bins=50,cmap='Blues',norm=LogNorm())
 #                       plt.plot(xyz_stars[:,0],xyz_stars[:,1],'yo',markersize=3)
-                        plt.axis([-75,75,-75,75])
+                        plt.axis([-width,width,-width,width])
                         plt.ylabel('Separation (kpc)')
                         plt.subplot(2,3,2)
-                        plt.hist2d(xyz_stars[:,1],xyz_stars[:,2],bins=50,cmap='Greys',norm=LogNorm())
+                        plt.hist2d(xyz_stars[:,1],xyz_stars[:,2],bins=50,cmap='Blues',norm=LogNorm())
 #                       plt.plot(xyz_stars[:,1],xyz_stars[:,2],'yo',markersize=3)
-                        plt.axis([-75,75,-75,75])
+                        plt.axis([-width,width,-width,width])
                         plt.subplot(2,3,3)
-                        plt.hist2d(xyz_stars[:,0],xyz_stars[:,2],bins=50,cmap='Greys',norm=LogNorm())
+                        plt.hist2d(xyz_stars[:,0],xyz_stars[:,2],bins=50,cmap='Blues',norm=LogNorm())
 #                       plt.plot(xyz_stars[:,0],xyz_stars[:,2],'yo',markersize=3)
-                        plt.axis([-75,75,-75,75])
+                        plt.axis([-width,width,-width,width])
 
                         # xy, yz, xz for the gas
                         plt.subplot(2,3,4)
-                        plt.hist2d(xyz_gas[:,0],xyz_gas[:,1],bins=50,cmap='Greys',norm=LogNorm())
-#                       pyn.plot.sph.image(sim.gas,qty='pos',width=75,cmap='Greys',norm=LogNorm())
+                        plt.hist2d(xyz_gas[:,0],xyz_gas[:,1],bins=50,cmap='YlOrRd',norm=LogNorm())
+#                       pyn.plot.sph.image(sim.gas,qty='pos',width=width,cmap='YlOrRd',norm=LogNorm())
 #                       plt.plot(xyz_gas[:,0],xyz_gas[:,1],'ro',markersize=2)
-#                       plt.axis([-75,75,-75,75])
+                        plt.axis([-width,width,-width,width])
                         plt.ylabel('Separation (kpc)')
                         plt.xlabel('Separation (kpc)')
                         plt.subplot(2,3,5)
-                        plt.hist2d(xyz_gas[:,1],xyz_gas[:,2],bins=50,cmap='Greys',norm=LogNorm())
+                        plt.hist2d(xyz_gas[:,1],xyz_gas[:,2],bins=50,cmap='YlOrRd',norm=LogNorm())
 #                       plt.plot(xyz_gas[:,1],xyz_gas[:,2],'ro',markersize=2)
-                        plt.axis([-75,75,-75,75])
+                        plt.axis([-width,width,-width,width])
                         plt.xlabel('Separation (kpc)')
                         plt.subplot(2,3,6)
 #                       plt.plot(xyz_gas[:,0],xyz_gas[:,2],'ro',markersize=2)
-                        plt.hist2d(xyz_gas[:,0],xyz_gas[:,2],bins=50,cmap='Greys',norm=LogNorm())
-                        plt.axis([-75,75,-75,75])
+                        plt.hist2d(xyz_gas[:,0],xyz_gas[:,2],bins=50,cmap='YlOrRd',norm=LogNorm())
+                        plt.axis([-width,width,-width,width])
                         plt.xlabel('Separation (kpc)')
 
 #                        # xy, yz, xz for the DM
 #                        plt.subplot(3,3,7)
 #                        plt.plot(xyz_dm[:,0],xyz_dm[:,1],'bo',markersize=1)
-#                        plt.axis([-75,75,-75,75])
+#                        plt.axis([-width,width,-width,width])
 #                        plt.ylabel('Separation (kpc)')
 #                        plt.xlabel('Separation (kpc)')
 #                        plt.subplot(3,3,8)
 #                        plt.plot(xyz_dm[:,1],xyz_dm[:,2],'bo',markersize=1)
-#                        plt.axis([-75,75,-75,75])
+#                        plt.axis([-width,width,-width,width])
 #                        plt.xlabel('Separation (kpc)')
 #                        plt.subplot(3,3,9)
 #                        plt.plot(xyz_dm[:,0],xyz_dm[:,2],'bo',markersize=1)
-#                        plt.axis([-75,75,-75,75])
+#                        plt.axis([-width,width,-width,width])
 #                        plt.xlabel('Separation (kpc)')
 
 # close the figure
@@ -91,7 +95,7 @@ if __name__ == "__main__":
                         
                 if opts.video:
                         import envoy
-                        print 'ffmpeg -y -qscale 1 -r %d -i %%09d.png %s' % (int(opts.fps), opts.outname)
-                        vid = envoy.run('ffmpeg  -qscale 1 -r %d -i %%09d.png %s' % (int(opts.fps), opts.outname))
-#                       for i in range(imgcount):
-#                               envoy.run("rm %09d.png" % (i))
+                        print 'ffmpeg -qscale 1 -r %d -i %%09d.png -y %s' % (int(opts.fps), opts.outname)
+                        vid = envoy.run('ffmpeg -qscale 1 -r %d -i %%09d.png -y %s' % (int(opts.fps), opts.outname))
+			for i in range(imgcount):
+				envoy.run("rm %09d.png" % (i))
