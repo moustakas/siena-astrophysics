2aab6f1a5b7028569681d855588bf28004b28407
thejackparticle <je08roge@siena.edu>
2015-06-16 13:38:17 -0400

    working on cutouts code

--- summer2015/Jack/grz_redmapper_cutouts.py
+++ summer2015/Jack/grz_redmapper_cutouts.py
@@ -14,12 +14,12 @@ def get_stamp(redshift):
     in_dir = os.getenv('HOME')+'/redmapper/' # push to a function
     out_dir = in_dir+'cutouts/'
     rmap = fits.getdata(in_dir+'rmap.fits',1)
-    theta = 1000.0*cosmo.arcsec_per_kpc_comoving(rmap['z']).value
+    theta = 350.0*cosmo.arcsec_per_kpc_comoving(rmap['z']).value
     stamp = theta/pixscale
     return stamp
 
 def main():
-    """This script will return a set of n .jpg and .fits files, which are cutouts      of galaxy clusters from the get_grz_redmapper.py script.
+    """This script will return a set of n .jpeg and .fits files, which are cutouts      of galaxy clusters from the get_grz_redmapper.py script.
     
     Cutout size is determined by the function get_stamp, which uses a cluster's red    shift to determine an optimal pixel width.
     
@@ -29,7 +29,7 @@ def main():
     
     rmap = fits.getdata(in_dir+'rmap.fits',1)
     
-    nrich = 1
+    nrich = 10
     rich = np.argsort(rmap['lambda_chisq'])[::-1][0:nrich]
     print('The top '+str(nrich)+' richest clusters: '+str(rmap['lambda_chisq'][rich]))
 
@@ -38,12 +38,12 @@ def main():
         ra = '{:6f}'.format(rmap['ra'][rich[ii]])
         dec = '{:6f}'.format(rmap['dec'][rich[ii]])
         cluster = 'cluster_{:06}'.format(rich[ii])
-        stamp = float('{:6f}'.format(get_stamp(rmap['z'])[rich[ii]]))
+        stamp = '{:n}'.format(np.floor(get_stamp(rmap['z'])[rich[ii]])/10*10)
         
         jpeg_url = 'http://imagine.legacysurvey.org/jpeg-cutout-decals-dr1?ra='+ra+'&dec='+dec+'&pixscale=2&size='+str(stamp)
         fits_url = 'http://imagine.legacysurvey.org/fits-cutout-decals-dr1?ra='+ra+'&dec='+dec+'&pixscale=2&size='+str(stamp)
-        print(jpeg_url)
-        print(fits_url)
+        print(jpeg_url, cluster)
+        print(fits_url, cluster)
         os.system('wget "'+jpeg_url+'" -O '+out_dir+cluster+'.jpg')
         os.system('wget "'+fits_url+'" -O '+out_dir+cluster+'.fits')
 
