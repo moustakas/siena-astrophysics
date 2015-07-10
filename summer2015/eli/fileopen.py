5aece4caef09ea71e58d819dca614858c5383a8e
Eli Beaudin <em29beau@siena.edu>
2015-07-10 14:15:00 -0400

    final commit for the summer

--- summer2015/eli/fileopen.py
+++ summer2015/eli/fileopen.py
@@ -5,15 +5,20 @@ import matplotlib.pylab as plt
 lado1=np.loadtxt('cmass_north_dr10.DD')
 
 #OUR INTERPRETATION
-lado2=np.loadtxt('DD_lado_tot.dat')
+lado2=np.loadtxt('method1.dat')
 
 lado11=np.flipud(lado1)
 lado111=lado1+lado11
-
+'''
 lado22=np.fliplr(lado2)
+lado220=lado2+lado22
+lado221=np.flipud(lado220)
+lado222=lado220+lado221
+'''
+lado22=np.flipud(lado2)
 lado222=lado2+lado22
 
-lado333=lado222-lado111
+lado333=lado111-lado222
 
 
 
@@ -22,14 +27,14 @@ rangeval=300
 extent=[-rangeval,rangeval,-rangeval,rangeval]
 
 plt.subplot(1,3,1)
-a=plt.imshow(lado1,extent=extent)
+a=plt.imshow(lado111,extent=extent)
 plt.title('Lado')
 plt.ylabel(r'$r_\perp (h^{-1}$Mpc)')
 plt.xlabel(r'$r_\parallel (h^{-1}$Mpc)')
 
 plt.colorbar()
 plt.subplot(1,3,2)
-b=plt.imshow(lado2,extent=extent)
+b=plt.imshow(lado222,extent=extent)
 plt.title('Our Interpretation')
 plt.ylabel(r'$r_\perp (h^{-1}$Mpc)')
 plt.xlabel(r'$r_\parallel (h^{-1}$Mpc)')
