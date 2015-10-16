098d18eeaf57af95c2d71e205deb5de76f0f1646
Eli Beaudin <em29beau@siena.edu>
2015-10-15 20:27:30 -0400

    editting main code

---
+++ research/eli/name_pending.py
@@ -0,0 +1,187 @@
+    parser= argparse.ArgumentParser()
+    parser.add_argument("infile1", help="Cmass data or mock data")
+    parser.add_argument("infile2", help="Cmass data or mock data")
+    parser.add_argument("--outfilename", default='default.dat', help="Outfile name")
+    parser.add_argument("--range1", default=None, type=str, help="Range for first infile, input as n-n")
+    parser.add_argument("--range2", default=None, type=str, help="Range for first infile, input as n-n")
+    parser.add_argument('--no-plots', dest='no_plots', default=False,action='store_true', help='do not generate plots')
+    parser.add_argument('--lado', dest='lado',default=False,action='store_true',help='Use Lado\'s calculations')
+    parser.add_argument('--pysurvey', dest='pysurvey',default=False,action='store_true',help='Use pysurvey\'s calculations')
+    parser.add_argument('--1d', dest='oned',default=False,action='store_true',help='One dimensional function')
+    args=parser.parse_args()
+
+    def main():
+    """Given command-line arguments, will return
+        frequency arrays for galactic distances.
+
+    Args:
+        See Below
+
+    Returns:
+        Dependant on command-line arguments.
+    """
+    parser= argparse.ArgumentParser()
+    parser.add_argument("infile1", help="Cmass data or mock data")
+    parser.add_argument("infile2", help="Cmass data or mock data")
+    parser.add_argument("--outfilename", default='default.dat', help="Outfile name")
+    parser.add_argument("--range1", default=None, type=str, help="Range for first infile, input as n-n")
+    parser.add_argument("--range2", default=None, type=str, help="Range for first infile, input as n-n")
+    parser.add_argument('--no-plots', dest='no_plots', default=False,action='store_true', help='do not generate plots')
+    parser.add_argument('--lado', dest='lado',default=False,action='store_true',help='Use Lado\'s calculations')
+    parser.add_argument('--pysurvey', dest='pysurvey',default=False,action='store_true',help='Use pysurvey\'s calculations')
+    parser.add_argument('--1d', dest='oned',default=False,action='store_true',help='One dimensional function')
+    args=parser.parse_args()
+
+    if args.no_plots:
+        plt.switch_backend('Agg')
+
+    infilename0 = args.infile1
+    infilename1 = args.infile2
+
+    outfilename = args.outfilename
+
+    range1lo = None
+    range1hi = None
+    if args.range1 is not None:
+        range1lo = int(args.range1.split('-')[0])
+        range1hi = int(args.range1.split('-')[1])
+
+    range2lo = None
+    range2hi = None
+    if args.range2 is not None:
+        range2lo = int(args.range2.split('-')[0])
+        range2hi = int(args.range2.split('-')[1])
+
+    do_diagonal = False
+    if range2lo>range1lo:
+        do_diagonal = True
+
+    # Check to see if we are using the same file for both (DD or RR)
+    # or if they are different (DR)
+    samefile = False
+    if (infilename0==infilename1):
+        samefile = True
+
+    # Randomizing a Sample of SDSS Data
+    ngals_for_calculation = 0
+    nrands=0
+    np.random.seed(1)
+
+    coords0 = jem.get_coordinates(infilename0,True,ngals_for_calculation,args.pysurvey)
+    coords1 = jem.get_coordinates(infilename1,True,nrands,args.pysurvey)
+    print 'Read in data files and coverted to cartesian!'
+
+
+    ngals0 = len(coords0)
+    ngals1 = len(coords1)
+
+    coords0cut = None
+    if range1lo is not None and range1hi is not None:
+        coords0cut = coords0[range1lo:range1hi]
+    else:
+        coords0cut = coords0
+
+    coords1cut = None
+    if range2lo is not None and range2hi is not None:
+        coords1cut = coords1[range2lo:range2hi]
+    else:
+        coords1cut = coords1
+   
+    # This is for the histogram.
+    nbins=200
+    rangeval=200
+
+    if args.oned:
+        nbins*=2
+
+    tot_freq = np.zeros((nbins,nbins)) 
+
+    
+    # Figure out the chunking.
+    
+
+    chunk_size = 50
+    nchunks = len(coords0cut)/chunk_size     #ngals_for_calculation/chunk_size
+
+    ncalcs_per_chunk = chunk_size*len(coords1cut) #chunk_size*ngals1
+
+    # These will store the calculations.
+    paras = np.zeros(ncalcs_per_chunk)
+    perps = np.zeros(ncalcs_per_chunk)
+
+    indexlo = 0
+    indexhi = 0
+
+    #Calculation Loop
+    for j in xrange(nchunks):
+        lo = j*chunk_size
+        hi = (j+1)*chunk_size
+        #print "Performing calculations for DR %d chunk: %d-%d" % (j,lo,hi)
+
+        paras *= 0.
+        perps *= 0.
+
+        #for i,r0 in enumerate(coords0[lo:hi]):
+        for i in range(lo,hi):
+            r0 = coords0cut[i]
+
+            lo1 = 0
+            if samefile:
+                lo1 = i
+                if do_diagonal==False:
+                    lo1 += 1
+
+            indexhi += len(coords1cut[lo1:])
+
+            other_gals = coords1cut[lo1:]
+
+            # Calc 1D using the pysurvey distance calc.
+            if args.lado==False and args.oned==False and args.pysurvey==True:
+                if len(other_gals)>0:
+                    temp_paras,temp_perps = jem.pysurvey_distance(r0,other_gals)
+
+            # Calc para and perp ``our" way. 
+            elif args.lado==False and args.oned==False and args.pysurvey==False:
+                temp_paras,temp_perps = jem.our_para_perp(r0,other_gals)
+
+            # Calc Lado's way
+            elif args.lado==True and args.oned==False and args.pysurvey==False:
+                temp_paras,temp_perps = jem.lado_para_perp(r0,other_gals)
+
+            # Calc just the 1D
+            elif args.lado==False and args.oned==True and args.pysurvey==False:
+                temp_paras,temp_perps = jem.one_dimension(r0,other_gals)
+
+            paras[indexlo:indexhi] = temp_paras
+            perps[indexlo:indexhi] = temp_perps
+
+            indexlo = indexhi
+        
+        # Histogram the values.
+        hist=plt.hist2d(perps[0:indexhi],paras[0:indexhi],bins=nbins,range=((-rangeval,rangeval),(-rangeval,rangeval)))
+        tot_freq += hist[0]
+        
+        # Mirror the negative perps
+        #hist=plt.hist2d(-1*perps[0:indexhi],paras[0:indexhi],bins=nbins,range=((-rangeval,rangeval),(-rangeval,rangeval)))
+        tot_freq += hist[0]
+
+        indexlo=0
+        indexhi=0
+
+        del hist
+
+        print tot_freq.sum()
+   
+    print 'Point:'
+    if args.no_plots==False:
+        print 'Final Plot'    
+        extent = [-rangeval,rangeval, -rangeval,rangeval]
+        fig = plt.figure()
+        axes = fig.add_subplot(1,1,1)
+        print 'Imshow'
+        print tot_freq
+        ret = axes.imshow(tot_freq,extent=extent,interpolation='nearest') #,origin=origin,cmap=cmap,axes=axes,aspect=aspect
+        plt.show()
+
+    print('Writing {}'.format(outfilename))
+    np.savetxt(outfilename,tot_freq)
