9ad4861a982cf12c6c94275fc11a42d9dfcfcf25
thejackparticle <je08roge@siena.edu>
2016-05-24 15:32:30 -0400

    refactored the code

--- summer2016/kevin/CUTE_trial_code.py
+++ summer2016/kevin/CUTE_trial_code.py
@@ -10,22 +10,8 @@ path='/home/desi1/repos/CUTE/CUTE/test/'
 # Load data file
 data=np.loadtxt(path+'corr_full_pm.dat')
 
-# Define empty matrices
-matrix0=np.zeros(len(data))
-matrix1=np.zeros(len(data))
-matrix2=np.zeros(len(data))
-matrix3=np.zeros(len(data))
-matrix4=np.zeros(len(data))
-matrix5=np.zeros(len(data))
-
-# Fill matrices with data
-for i in range(0,len(data)):
-    matrix0[i]=data[i][0]
-    matrix1[i]=data[i][1]
-    matrix2[i]=data[i][2]
-    matrix3[i]=data[i][3]
-    matrix4[i]=data[i][4]
-    matrix5[i]=data[i][5]
+for ii in range(0,len(data[0])):
+    locals()['matrix{0}'.format(ii)]=[item[ii] for item in data]
 
 # Plot data on loglog axes and show
 plt.loglog(matrix0,matrix1,'bo')
