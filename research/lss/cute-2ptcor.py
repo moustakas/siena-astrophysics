0abdb58b61f94578083b84426113343aaada86e3
thejackparticle <je08roge@siena.edu>
2016-05-27 15:31:42 -0400

    multiple changes

--- research/lss/cute-2ptcor.py
+++ research/lss/cute-2ptcor.py
@@ -4,8 +4,11 @@
 http://data.sdss3.org/sas/dr11/boss/lss/
 
 """
+#rperp para
+# all 600
+#monoppole vs published
 
-from __future__ import division, print_function
+From __future__ import division, print_function
 
 import os
 import sys
@@ -15,14 +18,50 @@ import logging as log
 import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def plotmqh(mono1,q1,hx1,rrange):
+
+    plt.figure()
+    plt.subplot(311)
+    plt.plot(rrange, mono1*rrange**2, 'ko')
+    plt.subplot(312)
+    plt.plot(rrange, q1*rrange**2, 'ko')
+    plt.subplot(313)
+    plt.plot(rrange, hx1*rrange**2, 'ko')
+
+def compute_monopole(mu, r, xirm):
+        xirm = xirm*1.0
+        Bxirm = np.reshape(xirm,[40,50])
+        xr = 0.025
+        mono1 = xr*np.trapz(Bxirm)
+        print(len(mono1))
+        return mono1
+
+def compute_quadrupole(mu, r, xirm):
+        xirm = xirm*(3*(mu*mu)-1.0)*(5/2)
+        Bxirm = np.reshape(xirm,[40,50])
+        xr = 0.025
+        q1 = xr*np.trapz(Bxirm)
+        return q1
+
+def compute_hexadecapole(mu, r, xirm):
+        xirm = xirm*(35*(mu*mu*mu*mu)-(30*mu*mu)+3)/8
+        Bxirm = np.reshape(xirm,[40,50])
+        xr = 0.025
+        hx1 = xr*np.trapz(Bxirm)
+        return hx1
+
+
 
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--dr', type=str, default='dr11', help='Specify the SDSS data release.')
     parser.add_argument('--parse', action='store_true', help='Parse the input datafiles.')
-    parser.add_argument('--docute', action='store_true', help='Run CUTE.')
+    parser.add_argument('--docute', type=str, default='3D_rm', help='Run CUTE.')
     parser.add_argument('--qaplots', action='store_true', help='Generate QAplots.')
+    # Add arguments concerning corr_type
 
     args = parser.parse_args()
 
@@ -47,13 +86,16 @@ def main():
 
         specz = allspecz[keep]
         ngal = len(keep)
+        print(specz[1])
 
         data = np.zeros((ngal,4))
       
 	data[:,0] = specz['RA']
 	data[:,1] = specz['DEC']
 	data[:,2] = specz['Z']
-	data[:,3] = specz['WEIGHT_FKP']*specz['WEIGHT_SYSTOT']*(specz['WEIGHT_NOZ']+specz['WEIGHT_CP']-1)
+	data[:,3] = specz['WEIGHT_SYSTOT']*(specz['WEIGHT_NOZ']+specz['WEIGHT_CP']-1)
+        # specz['WEIGHT_FKP']*specz['WEIGHT_SYSTOT']*(specz['WEIGHT_NOZ']+specz['WEIGHT_CP']-1)
+        # RETURN FPK WEIGHTS ONCE RANDOMS ARE COMPUTED
         log.info('Writing {}'.format(datafile))
 	np.savetxt(datafile, data)
 	
@@ -67,10 +109,11 @@ def main():
 	rand[:,1] = dec[keep]
 	rand[:,2] = z[keep]
 	rand[:,3] = wcp[keep]+wzf[keep]-1
+        # COMPUTE FPK WEIGHTS
 	
-        plt.figure()
-        plt.plot(data[:,0],data[:,1],'bo')
-        plt.show()
+        # plt.figure()
+        # plt.plot(data[:,0],data[:,1],'bo')
+        # plt.show()
 
         log.info('Writing {}'.format(randomfile))
 	np.savetxt(randomfile, rand)
@@ -78,52 +121,97 @@ def main():
     if args.docute:
         # Do stuff; write paramfile; call cute using os.system()
         pfile = open(paramfile,'w')
-        
-        pfile.write('data_filename= '+datafile+'\n')
-        pfile.write('random_filename= '+randomfile+'\n')
-        pfile.write('input_format= 2\n')
-        pfile.write('mask_filename= junk\n')
-        pfile.write('z_dist_filename= junk\n')
-        pfile.write('output_filename= '+outfile+'\n')
-        pfile.write('num_lines= all\n')
-        pfile.write('corr_type= monopole\n')
-        pfile.write('corr_estimator= LS\n')
-        pfile.write('np_rand_fact= 8\n')
-        pfile.write('omega_M= 0.3\n')
-        pfile.write('omega_L= 0.7\n')
-        pfile.write('w= -1\n')
-        pfile.write('log_bin= 0\n')
-        pfile.write('n_logint= 10\n')
-        pfile.write('dim1_max= 150\n')
-        pfile.write('dim1_nbin= 75\n')
-        pfile.write('dim2_max= 150\n')
-        pfile.write('dim2_nbin= 75\n')
-        pfile.write('dim3_min= 0.4\n')
-        pfile.write('dim3_max= 0.7\n')
-        pfile.write('dim3_nbin= 1\n')
-        pfile.write('radial_aperture= 1\n')
-        pfile.write('use_pm= 1\n')
-        pfile.write('n_pix_sph= 2048\n')
-     
+
+        if args.docute == 'monopole':
+            pfile.write('data_filename= '+datafile+'\n')
+            pfile.write('random_filename= '+randomfile+'\n')
+            pfile.write('input_format= 2\n')
+            pfile.write('mask_filename= junk\n')
+            pfile.write('z_dist_filename= junk\n')
+            pfile.write('output_filename= '+outfile+'\n')
+            pfile.write('num_lines= all\n')
+            pfile.write('corr_type= monopole\n')
+            pfile.write('corr_estimator= LS\n')
+            pfile.write('np_rand_fact= 8\n')
+            pfile.write('omega_M= 0.3\n')
+            pfile.write('omega_L= 0.7\n')
+            pfile.write('w= -1\n')
+            pfile.write('log_bin= 0\n')
+            pfile.write('n_logint= 10\n')
+            pfile.write('dim1_max= 150\n')
+            pfile.write('dim1_nbin= 75\n')
+            pfile.write('dim2_max= 150\n')
+            pfile.write('dim2_nbin= 75\n')
+            pfile.write('dim3_min= 0.4\n')
+            pfile.write('dim3_max= 0.7\n')
+            pfile.write('dim3_nbin= 1\n')
+            pfile.write('radial_aperture= 1\n')
+            pfile.write('use_pm= 1\n')
+            pfile.write('n_pix_sph= 2048\n')
+            
+        if args.docute == '3D_rm':
+            pfile.write('data_filename= '+datafile+'\n')
+            pfile.write('random_filename= '+randomfile+'\n')
+            pfile.write('input_format= 2\n')
+            pfile.write('mask_filename= junk\n')
+            pfile.write('z_dist_filename= junk\n')
+            pfile.write('output_filename= '+outfile+'\n')
+            pfile.write('num_lines= all\n')
+            pfile.write('corr_type= '+args.docute+'\n')
+            pfile.write('corr_estimator= LS\n')
+            pfile.write('np_rand_fact= 9.5217\n')
+            pfile.write('omega_M= 0.3\n')
+            pfile.write('omega_L= 0.7\n')
+            pfile.write('w= -1\n')
+            pfile.write('log_bin= 0\n')
+            pfile.write('n_logint= 0\n')
+            pfile.write('dim1_max= 200\n')
+            pfile.write('dim1_nbin= 50\n')
+            pfile.write('dim2_max= 1\n')
+            pfile.write('dim2_nbin= 40\n')
+            pfile.write('dim3_min= 0.4\n')
+            pfile.write('dim3_max= 0.7\n')
+            pfile.write('dim3_nbin= 1\n')
+            pfile.write('radial_aperture= 1\n')
+            pfile.write('use_pm= 0\n')
+            pfile.write('n_pix_sph= 2048\n')
+
         pfile.close()
-        #os.system('CUTE '+paramfile)
-        os.system('CUTE-noweights '+paramfile)
+        os.system('CUTE '+paramfile)
+        #os.system('CUTE-noweights '+paramfile)
     
     if args.qaplots:
         # Make rockin' plots and write out.
-        if corrtype = monopole
+        #if corr_type = monopole:
+            # rad, xi, xierr, DD, DR, RR = np.loadtxt(outfile, unpack=True)
+            # plt.figure()
+            # plt.scatter(rad, xi*rad**2)
+            # plt.axis([-5, 155, 0, 120])
+            # plt.xlabel('$\mathrm{\ r \ (Mpc)}$')
+            # plt.ylabel(r'$\mathrm{\ r^2 * \xi}$')
+            # plt.savefig(os.path.join('/home/work/projects/lss-boss/dr11', 'xi-with-weights.pdf'))
+            # plt.show()
 
-            rad, xi, xierr, DD, DR, RR = np.loadtxt(outfile, unpack=True)
-            plt.figure()
-            plt.scatter(rad, xi*rad**2)
-            #plt.axis([-5, 155, 0, 120])
-            plt.xlabel('$\mathrm{\ radius \ (Mpc)}$')
-            plt.ylabel(r'$\mathrm{\ radius^2 * \xi}$')
-            #plt.savefig(os.path.join('/home/work/projects/lss-boss/dr11', 'bao-no-weights.pdf'))
+        #if corr_type = 3D_ps
+            mu, rad, xi, xierr, DD, DR, RR = np.loadtxt(outfile, unpack=True)
+            rad = np.linspace(2,198,40)
+            mono1 = compute_monopole(mu, rad, xi)
+            q1 = compute_quadrupole(mu, rad, xi)
+            hex1 = compute_hexadecapole(mu, rad, xi)
+            plt.imshow(xi.reshape(50, 40)) ; plt.show()
+
+            # plt.figure()      
+            #ax = fig.add_subplot(111, projection='3d')
+            # plt.scatter(rad, mu, xi*rad**2)
+            # plt.figure()
+            # plt.mplot3D(rad, mu, xi*rad**2) 
+            # plt.axis([-5, 155, 0, 120])
+            # plt.xlabel('$\mathrm{\ r \ (Mpc)}$')
+            # plt.ylabel(r'$\mathrm{\ r^2 * \xi}$')
+            # plt.savefig(os.path.join('/home/work/projects/lss-boss/dr11', 'xi-with-weights.pdf'))
+            plotmqh(mono1,q1,hex1,rad)
             plt.show()
-
-        if corrtype = 3D_ps
-
+            
             
             
 if __name__ == "__main__":
