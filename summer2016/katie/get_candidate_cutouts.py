0260f0bfb9764893d1d2dff09c5ab790eb32be7f
Katie Hoag <kc12hoag@siena.edu>
2016-05-31 09:45:15 -0400

    Working on coding for cutotus

--- summer2016/katie/get_candidate_cutouts.py
+++ summer2016/katie/get_candidate_cutouts.py
@@ -2,5 +2,26 @@
 
 ''' This script will create a set of .jpg files which are cutouts of possible
 Planet nine candidates obtained from the planet9dr3.py script
+
 '''
 
+import os
+import numpy as np
+from astropy.io import fits
+
+
+def main():
+
+    in_dir = os.path.join(os.environ.get('HOME'), 'planet9-dr3-candidates.fits')
+    out_dir = in_dir + 'candidate_cutouts/'
+    
+
+    cutout_size = 20  # number of pixels per side of the cutout
+    
+
+
+
+
+
+
+    
