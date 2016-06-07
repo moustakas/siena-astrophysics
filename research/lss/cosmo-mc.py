dbf81f1f2266109b97e8fd6e386f4c138002457c
kjnapes <kj07napi@siena.edu>
2016-06-07 13:35:10 -0400

    generalizing

--- research/lss/cosmo-mc.py
+++ research/lss/cosmo-mc.py
@@ -9,8 +9,29 @@ import glob
 import logging as log
 import numpy as np
 
-# create directory and environment variable
-CosmoMC = os.path.join(os.getenv('COSMOMC'))
-outdir = os.path.join(Cosmo, 'cosmoMCout')
-paramfile = os.path.join(Cosmo, 'param')
+def main():
 
+    # create directory and environment variable
+    CosmoMC = os.path.join(os.getenv('COSMOMC'))
+    outdir = os.path.join(Cosmo, 'cosmoMCout')
+    paramfile = os.path.join(Cosmo, 'param')
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--docosmo', action='store_true', help='Run CosmoMC.')
+
+    args = parser.parse_args()
+
+    key = 'COSMOMC'
+    if key not in os.environ:
+        log.fatal('Required ${} environment variable not set'.format(key))
+        return 0
+
+    if args.docosmo:
+        # write the parameter file
+        pfile = open(paramfile, 'w')
+        pfile.write('\n')
+        pfile.close()            
+
+if __name__ == "__main__":
+    main()
