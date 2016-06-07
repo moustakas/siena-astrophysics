caad8edd05ab8f12bf2555c73ba94c530395e7eb
Katie Hoag <kc12hoag@siena.edu>
2016-06-07 10:20:07 -0400

    added a cut for asteroid data

--- summer2016/katie/get_candidate_cutouts.py
+++ summer2016/katie/get_candidate_cutouts.py
@@ -25,28 +25,31 @@ def main():
     """
 
     in_file = ('/home/desi2/candidatesp9/asteroids_decals_dr2.fits')
-    out_dir = os.path.join(os.environ.get('HOME'), 'candidate_cutouts/')
+    out_dir = os.path.join(os.environ.get('HOME'), 'asteroid_cutouts/')
 
     cand_info = fits_table(in_file)
     # Pre-select asteroids in the ra, dec box you know they exist.
-    ramin = ... # pick me!
-    ramax =
-    decmin =
-    decmax = 
-    these = np.where((cand_info.ra0>ramin)*(cand_info.ra0<ramax)*(cand_info.dec0>decmin)*(cand_info.dec0<decmax))
+    ramin = 107
+    ramax = 130
+    decmin = 16
+    decmax = 30
+    these = np.where((cand_info.ra0>ramin)*(cand_info.ra0<ramax)*
+                     (cand_info.dec0>decmin)*(cand_info.dec0<decmax))[0]
+    cand_info = cand_info[these]
 
     urls = []
     jpgfiles = []
-    for ii in range(50):
+    for ii in range(4):
         print('Working on candidate {}'.format(ii))
         ra = cand_info.ra0[ii]
         dec = cand_info.dec0[ii]
         
         jpgurl = 'http://legacysurvey.org/viewer/jpeg-cutout-decals-dr2?ra={:.4f}&dec={:.4f}&pixscale=0.262&size=200'.format(ra, dec)
         
-        jpgfile = 'obj-{:03d}.jpg'.format(ii)
-        grab = 'wget --continue -O "%s" "%s"' % (jpgfile, jpgurl)
-        
+        jpgfile = os.path.join(out_dir, 'obj-{:03d}.jpg'.format(ii))
+        grab = 'wget --continue -O {:s} {:s}' .format(jpgfile, jpgurl)
+        print(grab)
+        pdb.set_trace()  # Runs Python Debugger on code up to this line.   
         os.system(grab)
         if os.stat(jpgfile).st_size == 0:
             os.remove(jpgfile)
@@ -60,14 +63,6 @@ def main():
         print('<a {}"><img src="{:s}"></a>'.format(thisurl, thisjpg))
     print('</body></html>')
 
-    pdb.set_trace()  # Runs Python Debugger on code up to this line.
-
-    print('<html><body>')
-    for ii,(jpg_url,fn) in enumerate(zip(goodurls, images)):
-        print('<a href="http://legacysurvey.org/viewer/?ra=%.4f&dec=%.4f"><img src="%s"></a>' % (ra, dec, fn))
-    print('</body></html>')
-
-    pdb.set_trace()  # Runs Python Debugger on code up to this line.
 
 
 if __name__ == '__main__':
