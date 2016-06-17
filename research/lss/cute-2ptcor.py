35c28a8a49b305b7b3c0bfa8a1899407dbcfef49
kjnapes <kj07napi@siena.edu>
2016-06-17 11:22:12 -0400

    push

--- research/lss/cute-2ptcor.py
+++ research/lss/cute-2ptcor.py
@@ -11,7 +11,6 @@ import sys
 import argparse
 import glob
 import logging as log
-import pdb
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,34 +18,29 @@ from astropy.io import fits
 from astropy.cosmology import WMAP7
 from matplotlib.colors import LogNorm
 
-def plotmqh(mono1,q1,hx1,rrange):
-    #plt.figure()
-    #plt.subplot(111)
-    plt.plot(rrange, mono1*rrange**2, 'ko')
-    #plt.subplot(212)
-    #plt.plot(rrange, -q1*rrange**2, 'ko')
-    #plt.show()
+def plotmqh(monopole,quadrupole,hexadecapole,rrange):
+    plt.plot(rrange, monopole*rrange**2, 'ko')
 
 def compute_monopole(mu, r, xirm):
     xirm = xirm*1.0
-    Bxirm = np.reshape(xirm,[40,50])# generalize
-    xr = 0.025
-    mono1 = xr*np.trapz(Bxirm, axis=0)
-    return mono1
+    Bxirm = np.reshape(xirm,[40,50]) # generalize
+    xr = 0.025 # find out about this factor (factor of 1/40)
+    monopole = xr*np.trapz(Bxirm, axis=0) # go through the math of why
+    return monopole
 
 def compute_quadrupole(mu, r, xirm):
     xirm = xirm*(3*(mu*mu)-1.0)*(5/2)
     Bxirm = np.reshape(xirm,[40,50])
     xr = 0.025
-    q1 = xr*np.trapz(Bxirm)
-    return q1
+    quadrupole = xr*np.trapz(Bxirm)
+    return quadrupole
 
 def compute_hexadecapole(mu, r, xirm):
     xirm = xirm*(35*(mu*mu*mu*mu)-(30*mu*mu)+3)/8
     Bxirm = np.reshape(xirm,[40,50])
     xr = 0.025
-    hx1 = xr*np.trapz(Bxirm)
-    return hx1
+    hexadecapole = xr*np.trapz(Bxirm)
+    return hexadecapole
 
 def calc_fkp_weights(z, zmin, zmax): 
     NRB = 200 
@@ -92,7 +86,7 @@ def main():
     parser.add_argument('--parse', action='store_true', help='Parse the input datafiles.')
     parser.add_argument('--docute', action='store_true', help='Run CUTE.')
     parser.add_argument('--qaplots', action='store_true', help='Generate QAplots.')
-    parser.add_argument('--type', type=str, default='monopole', help='Specify Correlation Type.')
+    parser.add_argument('--corrtype', type=str, default='monopole', help='Specify Correlation Type.')
     parser.add_argument('--cosmo', type=int, default=1, help='Select cosmology')
 
     args = parser.parse_args()
@@ -107,16 +101,16 @@ def main():
     randomsdir = os.path.join(os.getenv('LSS_BOSS'), args.dr, 'randoms')
     datafile = os.path.join(drdir, args.dr+'_cmass.dat')
     randomfile = os.path.join(drdir, 'parsed', args.dr+'_cmass_random')
-    outfile = os.path.join(drdir, 'cuteout', args.type, 'dr11_2pt_'+args.type+'_')
-    paramfile = os.path.join(drdir, 'param', 'dr11_'+args.type+'_')
+    outfile = os.path.join(drdir, 'cuteout', args.corrtype, 'dr11_2pt_'+args.corrtype+'_')
+    paramfile = os.path.join(drdir, 'param', 'dr11_'+args.corrtype+'_')
     randomslist = glob.glob(os.path.join(randomsdir, '*.dat'))
 
-    if args.type == '3D_ps':
+    if args.corrtype == '3D_ps':
+
         npibins = 150
         nsigbins = 150
         maxpi = 150
         maxsig = 150
-            
 
     # Parse the input data and write out CUTE-compatible files.
     if args.parse:
@@ -125,13 +119,12 @@ def main():
         keep = np.where((allspecz['Z']>0.43)*(allspecz['Z']<0.7))[0]
         specz = allspecz[keep]
         ngal = len(keep)
-        my_data_wfkp = calc_fkp_weights(specz['Z'], 0.43, 0.7)
+        datafkp = calc_fkp_weights(specz['Z'], 0.43, 0.7)
         data = np.zeros((ngal,4))
         data[:,0] = specz['RA']
         data[:,1] = specz['DEC']
         data[:,2] = specz['Z']
-        data[:,3] = my_data_wfkp*specz['WEIGHT_SYSTOT']*(specz['WEIGHT_NOZ']+specz['WEIGHT_CP']-1)
-                    # specz['WEIGHT_FKP']*specz['WEIGHT_SYSTOT']*(specz['WEIGHT_NOZ']+specz['WEIGHT_CP']-1)
+        data[:,3] = datafkp*specz['WEIGHT_SYSTOT']*(specz['WEIGHT_NOZ']+specz['WEIGHT_CP']-1)
         print('Writing {}'.format(datafile))
         log.info('Writing {}'.format(datafile))
         np.savetxt(datafile, data)
@@ -147,8 +140,8 @@ def main():
             rand[:,2] = z[keep]
             wfkp = calc_fkp_weights(rand[:,2], 0.43, 0.7)
             rand[:,3] = wfkp*(wcp[keep]+wzf[keep]-1)
-            log.info('Writing {}'.format(randomfile+'_'+args.type+'_fkp_{}.dat'.format(item+4001)))
-            print('Writing {}'.format(randomfile+'_'+args.type+'_fkp_{}.dat'.format(item+4001)))
+            log.info('Writing {}'.format(randomfile+'_'+args.corrtype+'_fkp_{}.dat'.format(item+4001)))
+            print('Writing {}'.format(randomfile+'_'+args.corrtype+'_fkp_{}.dat'.format(item+4001)))
             np.savetxt(randomfile+'_fkp_{}.dat'.format(item+4001), rand) # rename all of the randomefile+3D_rm files
                       
     if args.docute:
@@ -161,7 +154,7 @@ def main():
             omega_M = 0.274
             omega_L = 1 - omega_M
     
-        for item in range(1):#len(randomslist)):
+        for item in range(len(randomslist)):
             # Create a unique filename for each parameeter file
             newfile = paramfile+'fkp_{}.param'.format(item+4001)
 
@@ -172,18 +165,18 @@ def main():
             pfile.write('mask_filename= junk\n')
             pfile.write('z_dist_filename= junk\n')
             pfile.write('output_filename= '+outfile+'fkp_{}.dat'.format(item+4001)+'\n')
-            pfile.write('corr_type= '+args.type+'\n')
+            pfile.write('corr_type= '+args.corrtype+'\n')
             pfile.write('num_lines= all\n')
             pfile.write('corr_estimator= LS\n')
             
-            if args.type == 'monopole':
+            if args.corrtype == 'monopole':
                 pfile.write('input_format= 2\n')
                 pfile.write('np_rand_fact= 8\n')
                 pfile.write('omega_M= {}\n'.format(omega_M))
                 pfile.write('omega_L= {}\n'.format(omega_L))
                 pfile.write('w= -1\n')
                 pfile.write('log_bin= 1\n') # changed from 0 to 1
-                pfile.write('n_logint= 10\n')
+                pfile.write('n_logint= 23\n')
                 pfile.write('dim1_max= 150\n')
                 pfile.write('dim1_nbin= 75\n')
                 pfile.write('dim2_max= 150\n')
@@ -195,7 +188,7 @@ def main():
                 pfile.write('use_pm= 1\n')
                 pfile.write('n_pix_sph= 2048\n')
                 
-            if args.type == '3D_rm':
+            if args.corrtype == '3D_rm':
                 pfile.write('input_format= 2\n')
                 pfile.write('np_rand_fact= 9.5217\n')
                 pfile.write('omega_M= {}\n'.format(omega_M))
@@ -203,10 +196,10 @@ def main():
                 pfile.write('w= -1\n')
                 pfile.write('log_bin= 1\n') # changed from 0 to 1
                 pfile.write('n_logint= 0\n')
-                pfile.write('dim1_max= 200\n')
+                pfile.write('dim1_max= 160\n')
                 pfile.write('dim1_nbin= 50\n')
                 pfile.write('dim2_max= 1\n')
-                pfile.write('dim2_nbin= 40\n')
+                pfile.write('dim2_nbin= 200\n')
                 pfile.write('dim3_min= 0.4\n')
                 pfile.write('dim3_max= 0.7\n')
                 pfile.write('dim3_nbin= 1\n')
@@ -214,7 +207,7 @@ def main():
                 pfile.write('use_pm= 0\n')
                 pfile.write('n_pix_sph= 2048\n')
 
-            if args.type == '3D_ps':
+            if args.corrtype == '3D_ps':
                 pfile.write('input_format= 2\n')
                 pfile.write('np_rand_fact= 9.5217\n')
                 pfile.write('omega_M= {}\n'.format(omega_M))
@@ -242,7 +235,7 @@ def main():
         anderson1 = os.path.join(drdir, 'Anderson_2013_CMASSDR11_corrfunction_x0x2_prerecon.dat')
         and_rad,and_mono,and_quad = np.loadtxt(anderson1, unpack=True)
 
-        if args.type == 'monopole':
+        if args.corrtype == 'monopole':
             for item in range(len(randomslist)):
                 thisout = outfile+'{}.dat'.format(item+4001)
                 rad, xi, xierr, DD, DR, RR = np.loadtxt(thisout, unpack=True)
@@ -253,22 +246,22 @@ def main():
                 #plt.savefig(os.path.join(drdir,'qaplots','power_spectrum_monopole_{}.png'.format(item+4001)))
                 plt.show()
                 
-        if args.type == '3D_rm':
+        if args.corrtype == '3D_rm':
             for item in range(len(randomslist)):
                 thisout = outfile+'fkp_{}.dat'.format(item+4001)
                 mu, rad, xi, xierr, DD, DR, RR = np.loadtxt(thisout, unpack=True)
                 rad = np.linspace(2, 198, 40)
                 # rad = np.linspace(2, 198, 40)
                 # rad = rad.reshape((50,40))
-                mono1 = compute_monopole(mu, rad, xi)
-                q1 = compute_quadrupole(mu, rad, xi)
-                plotmqh(mono1,q1,hex1,rad)
+                monopole = compute_monopole(mu, rad, xi)
+                quadrupole = compute_quadrupole(mu, rad, xi)
+                plotmqh(monopole,quadrupole,hex1,rad)
                 plt.xlabel('$\mathrm{\ r \ (Mpc \,  h^{-1})}$')
                 plt.ylabel(r'$\mathrm{\ r^2 \xi(r)}$')
             plt.plot(and_rad, (and_mono)*and_rad**2, 'r-')
             plt.show()
                 
-        if args.type == '3D_ps':
+        if args.corrtype == '3D_ps':
             xi = np.zeros((len(randomslist), nsigbins, npibins))
             for item in range(len(randomslist)):
                 thisout = outfile+'fkp_{}.dat'.format(item+4001)
@@ -276,7 +269,7 @@ def main():
                 xi = thisxi.reshape(nsigbins, npibins)
             xi = np.mean(xi)
             bigxi = np.zeros((nsigbins*2, npibins*2))
-            xi2d = xi.reshape(75, 75)
+            xi2d = xi.reshape(nsigbins, npibins)
             bigxi[:nsigbins, :npibins] = np.fliplr(np.flipud(xi2d))
             bigxi[nsigbins:2*nsigbins, :npibins] = np.fliplr(xi2d)
             bigxi[:nsigbins, npibins:2*npibins] = np.flipud(xi2d)
